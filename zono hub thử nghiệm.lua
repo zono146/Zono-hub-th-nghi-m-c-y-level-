b49c70c7873445833ae6bf9971d549931e7ab4b2
zono146 <silihiman123543@gmail.com>
2026-04-01 20:52:28 +0700

    Refactor zono hub script with new UI elements

--- zono hub thử nghiệm.lua
+++ zono hub thử nghiệm.lua
@@ -1,41 +1,63 @@
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V2/refs/heads/main/UiREDzV2.lua")))()
-
-       local Window = MakeWindow({
-         Hub = {
-         Title = "zono hub",
-         Animation = "zono hub"
-         },
-        Key = {
-        KeySystem = true,
-        Title = "zono Key",
-        Description = "",
-        KeyLink = "zonohub-key",
-        Keys = {"zonohub-key"},
-        Notifi = {
-        Notifications = true,
-        CorrectKey = "Running the Script...",
-       Incorrectkey = "The key is incorrect",
-       CopyKeyLink = "Copied to Clipboard"
-      }
-    }
-  })
-
-       MinimizeButton({
-       Image = "http://www.roblox.com/asset/?id=93280542413490",
-       Size = {30, 30},
-       Color = Color3.fromRGB(10, 10, 10),
-       Corner = true,
-       Stroke = false,
-       StrokeColor = Color3.fromRGB(255, 0, 0)
-      })
-      ------ Tab
-     local Tab1o = MakeTab({Name = "cày"})
-     ------- BUTTON
-    
-    AddButton(Tab1o, {
-     Name = "cày level",
-    Callback = function()
-	  local Settings = {
-  JoinTeam = "Pirates"; -- Pirates/Marines
-  Translator = true; -- true/false
-}
+local Logo = Instance.new("ImageLabel")
+local TextButton = Instance.new("TextButton")
+local UICorner_2 = Instance.new("UICorner")
+--Properties:
+BladLock.Name = "BladLock"
+BladLock.Parent = game.CoreGui
+BladLock.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+Frame.Parent = BladLock
+Frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Frame.BorderSizePixel = 0
+Frame.Position = UDim2.new(0.133798108, 0, 0.20107238, 0)
+Frame.Size = UDim2.new(0, 202, 0, 70)
+Frame.Active = true
+Frame.Draggable = true
+local function TopContainer()
+Frame.Position = UDim2.new(0.5, -Frame.AbsoluteSize.X / 2, 0, -Frame.AbsoluteSize.Y / 2)
+end
+TopContainer()
+Frame:GetPropertyChangedSignal("AbsoluteSize"):Connect(TopContainer)
+UICorner.Parent = Frame
+Logo.Name = "Logo"
+Logo.Parent = Frame
+Logo.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Logo.BackgroundTransparency = 5.000
+Logo.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Logo.BorderSizePixel = 0
+Logo.Position = UDim2.new(0.326732665, 0, 0, 0)
+Logo.Size = UDim2.new(0, 70, 0, 70)
+Logo.Image = "rbxassetid://16792732223"
+Logo.ImageTransparency = 0.200
+TextButton.Parent = Frame
+TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextButton.BackgroundTransparency = 5.000
+TextButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextButton.BorderSizePixel = 0
+TextButton.Position = UDim2.new(0.0792079195, 0, 0.18571429, 0)
+TextButton.Size = UDim2.new(0, 170, 0, 44)
+TextButton.Font = Enum.Font.SourceSansSemibold
+TextButton.Text = "Toggle CamLock"
+TextButton.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextButton.TextScaled = true
+TextButton.TextSize = 14.000
+TextButton.TextWrapped = true
+local state = true
+TextButton.MouseButton1Click:Connect(
+function()
+state = not state
+if not state then
+TextButton.Text = "ON"
+CamlockState = true
+enemy = FindNearestEnemy()
+else
+TextButton.Text = "OFF"
+CamlockState = false
+enemy = nil
+end
+end
+)
+-- Function to hide the loading screen after a certain duration
+local function HideLoadingScreen()
+LoadingScreen:Destroy()
+end
